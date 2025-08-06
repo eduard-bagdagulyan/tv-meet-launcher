2b10f3ad03a362c889317ea2c0dfca7c782266b2
Eduard Bagdagulyan <eduard.bagdagulyan@listgroup.am>
2025-08-06 18:16:48 +0400

    fixes

--- idle-launcher.js
+++ idle-launcher.js
@@ -1,7 +1,9 @@
 const {chromium} = require('playwright');
 
+const USER_DATA_DIR = './google-profile';
+
 async function showIdleScreen() {
-    const context = await chromium.launch({
+    const context = await chromium.launchPersistentContext(USER_DATA_DIR,{
         headless: false,
         args: [
             '--start-maximized',
